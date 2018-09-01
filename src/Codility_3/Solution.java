9361e2e1944bb7b889f860a77970ed210b844332
Adrian Lachowicz <adrianlachowicz@o2.pl>
2018-09-01 18:03:37 +0200

    Codility_3_Prepared

--- src/Codility_3/Solution.java
+++ src/Codility_3/Solution.java
@@ -1,4 +1,15 @@
 package Codility_3;
 
-public class Solution {
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+    public int solution(int[] A)
+    {
+        return 0;
+    }
+
+
 }
